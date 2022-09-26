0e50d0515de842fb11feb0e645e338994ed94ad1
Sumit <ss4366@njit.edu>
2022-09-26 17:50:12 -0400

    adding back php file

---
+++ dblog.php
@@ -0,0 +1,11 @@
+<?php
+   //database connection
+   $server = 'sql1.njit.edu';
+   $dbuser = '******';
+   $pass = '*************';
+   $dbname = 'ss4366';
+   $database = mysqli_connect($server, $dbuser, $pass, $dbname);
+   if(mysqli_connect_errno()){
+   	die("connection error".mysqli_connect_error());
+    }
+?>
