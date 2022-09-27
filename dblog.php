c98c22f9b6043ac49e47c6b6f9a1c0654b97c1ee
Sumit <ss4366@njit.edu>
2022-09-27 01:55:25 -0400

    change in dblog.php file

--- dblog.php
+++ dblog.php
@@ -4,9 +4,8 @@
    $server = 'sql1.njit.edu';
    $dbuser = 'ss4366';
    $pass = 'Ss!98119811';
-   $dbname = 'information';
+   $dbname = 'ss4366';
    $database = mysqli_connect($server, $dbuser, $pass, $dbname);
    if(mysqli_connect_errno()){
    	die("connection error".mysqli_connect_error());
     }
-?>
