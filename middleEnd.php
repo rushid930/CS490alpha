145f892a4e496e22bef21cb8cedea550560f656e
rushid930 <rushid930@gmail.com>
2022-09-29 03:28:42 -0400

    edited everything to connect parts. untested

--- middleEnd.php
+++ middleEnd.php
@@ -1,8 +1,7 @@
 <?php
 
-
-$backUrl = 'https://web.njit.edu/~ss4366/back-end.php';
-$frontUrl = 'https://web.njit.edu/~rd448/login.php';
+$backUrl = 'https://web.njit.edu/~rd448/backEnd.php';
+$frontUrl = 'https://web.njit.edu/~rd448/frontEnd.php';
 $username = $_POST['username'];
 $password = $_POST['password'];
 
@@ -25,17 +24,29 @@ curl_setopt($chBack, CURLOPT_URL, '$backUrl');
 curl_setopt($chBack, CURLOPT_RETURNTRANSFER, true);
 curl_setopt($chBack, CURLOPT_POST, 1);
 curl_setopt($chBack, CURLOPT_POSTFIELDS, $login);
-$responeB = curl_exec($chBack);
+$responseB = curl_exec($chBack);
 curl_close($chBack);
 
 
-if(isset($username && $password)){
+/*if(isset($username) && isset($password)){
     $message = 'Authenticated';
 } 
 else{
     $message = 'Denied';
 }
+*/
 
-json_decode($message);
+if(strpos($responseF, "Invalid Username")==false){
+    $message = 'Authenticated';
+}
+else{
+    $message = 'Denied';
+}
+
+$decoded_json = json_decode($responseB, true);
+$decoded_json['respNJIT'] = $message;
+//json_decode($message);
+$finalJSON = json_encode($decoded_json, JSON_PRETTY_PRINT);
+echo $finalJSON;
 
 ?>
