e97b5e51bbebddd3366dfe5d5507ee42dc15dbfd
Sumit <ss4366@njit.edu>
2022-09-29 03:54:27 -0400

    dblog.php edit

--- dblog.php
+++ dblog.php
@@ -1,11 +1,15 @@
 <?php
-   //database connection
-   //test commit
-   $server = 'sql1.njit.edu';
-   $dbuser = 'ss4366';
-   $pass = 'Ss!98119811';
-   $dbname = 'ss4366';
-   $database = mysqli_connect($server, $dbuser, $pass, $dbname);
-   if(mysqli_connect_errno()){
-   	die("connection error".mysqli_connect_error());
-    }
+//database connection
+$server = 'sql1.njit.edu';
+$dbuser = 'ss4366';
+$pass = 'Ss!98119811';
+$dbname = 'ss4366';
+$database = mysqli_connect($server, $dbuser, $pass, $dbname);
+if (mysqli_connect_errno()) {
+   die("connection error" . mysqli_connect_error());
+}
+?>
+
+<?php
+
+?>
