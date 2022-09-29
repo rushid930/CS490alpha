90a4cdeeeb26ccef52125ea5d27be905eb4bf310
Sumit <ss4366@njit.edu>
2022-09-29 16:39:27 -0400

    authCode added

--- dblog.php
+++ dblog.php
@@ -1,15 +1,15 @@
 <?php
-//database connection
-$server = 'sql1.njit.edu';
-$dbuser = 'ss4366';
-$pass = 'Ss!98119811';
-$dbname = 'ss4366';
-$database = mysqli_connect($server, $dbuser, $pass, $dbname);
-if (mysqli_connect_errno()) {
-   die("connection error" . mysqli_connect_error());
-}
-?>
+   //database connection
+   error_reporting(E_ERROR | E_WARNING | E_PARSE | E_NOTICE);  
+   ini_set('display_errors' , 1);
 
-<?php
+   $server = 'sql1.njit.edu';
+   $dbuser = 'ss4366';
+   $pass = 'Ss!98119811';
+   $dbname = 'ss4366';
+   $database = mysqli_connect($server, $dbuser, $pass, $dbname);
 
+   if(mysqli_connect_errno($database)){
+    die("connection error".mysqli_connect_error($database));
+   }
 ?>
